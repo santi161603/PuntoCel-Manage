434b7313e71e1edf5c13166503a51037485cca2f
santi161603 <116686563+santi161603@users.noreply.github.com>
2024-10-21 12:29:35 -0500

    drawable

--- app/src/main/java/com/uni/proyecto/event/MainActivity.kt
+++ app/src/main/java/com/uni/proyecto/event/MainActivity.kt
@@ -1,19 +1,33 @@
 package com.uni.proyecto.event
 
 import android.app.AlertDialog
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.uni.proyecto.event.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var dialog: AlertDialog? = null
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,18 +35,63 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.fragment_activity_main)
+        navView.setupWithNavController(navController)
+
+        drawerLayout = binding.container
+
+        // Configurar el NavigationView
+        val navViewTwo: NavigationView = findViewById(R.id.nav_view_dra)
+        navViewTwo.setupWithNavController(navController)
+        navViewTwo.setNavigationItemSelectedListener { menuItem ->
+            // Manejar clic en elementos del menú
+            when (menuItem.itemId) {
+                R.id.nav_item_1 -> {
+                    // Acciones para el Item 1
+                }
+                R.id.nav_item_2 -> {
+                    // Acciones para el Item 2
+                }
+                R.id.nav_item_3 -> {
+                    // Acciones para el Item 3
+                }
+            }
+            // Cerrar el drawer al seleccionar un item
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
 
         val appBarConfiguration = AppBarConfiguration(
-            setOf(
+           setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
-
-       // setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 
     fun ShowAlert(mss: String,onClickResult: (() -> Unit)? = null) {
         val builder = AlertDialog.Builder(this)
